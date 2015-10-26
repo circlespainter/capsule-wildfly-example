c607a7c533d7e9859f92b327329342c60dd83023
circlespainter <fabio@paralleluniverse.co>
2015-10-26 14:52:28 +0200

    Remove unused script

--- boot-shield.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export PATH=$PATH:/java/bin/
-mkdir -p /var/lib/
-cp -arf /capsule/app/wildfly-10.0.0.CR4 /var/lib/
-export JBOSS_HOME=/var/lib/wildfly-10.0.0.CR4
-bash /var/lib/wildfly-10.0.0.CR4/bin/standalone.sh $@
