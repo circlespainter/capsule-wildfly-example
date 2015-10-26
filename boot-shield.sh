e52aef95dd73bd2d1d22318a532d3c59eb61647d
circlespainter <fabio@paralleluniverse.co>
2015-10-26 14:33:26 +0200

    Fix server image copying to writable area

--- boot-shield.sh
+++ boot-shield.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 export PATH=$PATH:/java/bin/
-cp -Rf /capsule/app/wildfly-10.0.0.CR4 /var/lib
+mkdir -p /var/lib/
+cp -arf /capsule/app/wildfly-10.0.0.CR4 /var/lib/
 export JBOSS_HOME=/var/lib/wildfly-10.0.0.CR4
-. /var/lib/wildfly-10.0.0.CR4/bin/standalone.sh
+bash /var/lib/wildfly-10.0.0.CR4/bin/standalone.sh $@
